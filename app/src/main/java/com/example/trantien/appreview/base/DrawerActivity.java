72853da1864770ab6e573d9a2bda0933f3927984
Tran Tien <trungtien.uit@gmail.com>
2018-05-03 06:45:44 +0700

    init

---
+++ app/src/main/java/com/example/trantien/appreview/base/DrawerActivity.java
@@ -0,0 +1,5 @@
+package com.example.trantien.appreview.base;
+
+public class DrawerActivity extends Base {
+
+}
---
+++ app/src/main/java/com/example/trantien/appreview/base/IActivityListener.java
@@ -0,0 +1,14 @@
+package com.example.trantien.appreview.base;
+
+interface IActivityListener {
+
+    void showBarLoading();
+
+    void hideBarLoading();
+
+    void showLoading();
+
+    void hideLoading();
+
+}
+
